f1e774a70199eee8652c525ebddab14e7b8e8f00
JUNED <aj215210@gmail.com>
2025-07-20 13:59:50 +0530

    feat(ui): added star

--- client/src/components/app/Hero.tsx
+++ client/src/components/app/Hero.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { Spotlight } from "@/components/mini/spotlight-new";
 import { Button } from "../ui/button";
+import { StarIcon } from "lucide-react";
 
 export function Hero() {
   return (
@@ -9,6 +10,15 @@ export function Hero() {
       <Spotlight />
 
       <div className=" p-4 mt-12 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
+        <div className="flex justify-center items-center pb-6 gap-4">
+          <Button
+            variant="outline"
+            className="rounded-4xl group flex items-center gap-2"
+          >
+            Give us a Star
+            <StarIcon className="h-6 w-6 text-gray-400 group-hover:text-yellow-400 group-hover:fill-yellow-400 transition-all duration-300" />
+          </Button>
+        </div>
         <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
           The Easiest Way to Ship Code
         </h1>
